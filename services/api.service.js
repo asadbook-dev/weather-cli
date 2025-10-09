1ee28c4ec7a20d595920fb53f0b697a4c6793a16
asadbek <asadbekkhudayberdiyev07@gmail.com>
2025-10-09 15:17:47 +0900

    Axios

--- services/api.service.js
+++ services/api.service.js
@@ -1,31 +1,43 @@
-import https from "https";
+// import https from "https";
+import axios from "axios";
 import { TOKEN_DICTIONARY, getKeyValue } from "./storage.service.js";
 
 const getWeather = async (city) => {
-  // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
   const token = await getKeyValue(TOKEN_DICTIONARY.token);
   if (!token) {
     throw new Error("API does not exist, -t [API_KEY] for saving token ");
   }
-  const url = new URL("https://api.openweathermap.org/data/2.5/weather");
 
-  url.searchParams.append("q", city);
-  url.searchParams.append("appid", token);
-  url.searchParams.append("lan", "en");
-  url.searchParams.append("units", "metric");
+  const { data } = await axios.get(
+    "https://api.openweathermap.org/data/2.5/weather",
+    {
+      params: {
+        q: city,
+        appid: token,
+        lan: "en",
+        units: "metric",
+      },
+    }
+  );
 
-  console.log(url);
+  return data;
 
-  https.get(url, (response) => {
-    let res = "";
-    response.on("data", (chunk) => {
-      res += chunk;
-    });
-    response.on("end", () => {
-      console.log(res);
-    });
-  });
+  // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+  // const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+  // url.searchParams.append("q", city);
+  // url.searchParams.append("appid", token);
+  // url.searchParams.append("lan", "en");
+  // url.searchParams.append("units", "metric");
+  // console.log(url);
+  // https.get(url, (response) => {
+  //   let res = "";
+  //   response.on("data", (chunk) => {
+  //     res += chunk;
+  //   });
+  //   response.on("end", () => {
+  //     console.log(res);
+  //   });
+  // });
 };
 
 export { getWeather };
